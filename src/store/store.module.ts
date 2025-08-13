8ed7dd7a1e254ba3adfc91c8b206955ec1180144
Winner Le Paradoxe Konou <wilpar49@hotmail.com>
2025-08-13 11:32:51 +0000

    module name mapper

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -4,9 +4,9 @@ import { StoreController } from './store.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Store, StoreSchema } from './entities/store.entity';
 import { Currency, CurrencySchema } from 'src/currency/entities/currency.entity';
-import { User, UserSchema } from 'src/user/entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from 'src/auth/auth.module';
+import { User, UserSchema } from 'src/user/entities/user.entity';
 
 @Module({
 
